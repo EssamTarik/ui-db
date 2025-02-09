a32f892ed46f88b8b61175436aed6c22f0a4a482
EssamTarik <essamtarik2000@gmail.com>
2025-02-09 12:20:17 +0100

    GH pages deploy

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
 export const UI_DB_PUBLIC_URL =
   'https://static.ui.com/fingerprint/ui/public.json';
+
+export const BASE_NAME = import.meta.env.VITE_BASE_NAME;
--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,11 @@ import '@fontsource/lato';
 
 import './index.css';
 import App from './App.tsx';
+import { BASE_NAME } from './consts.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_NAME || ''}>
       <App />
     </BrowserRouter>
   </StrictMode>
